99944fd4a7332fae6f9579fb084d73266e563ebb
Luccas Caron <luccascaron1@gmail.com>
2023-03-10 14:41:59 -0300

    To-Do-List
    To Do List com JavaScript

---
+++ js/script.js
@@ -0,0 +1,84 @@
+//Seleção de Elementos
+
+    const todoform = document.querySelector("#todo-form");
+    const todoInput = document.querySelector("#todo-Input");
+    const todoList = document.querySelector("#todo-list");
+    const editForm = document.querySelector("#edit-form");
+    const editInput = document.querySelector("#edit-input");
+    const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+    
+//Funções
+
+const saveTodo = (text) => {
+
+    const todo = document.createElement("div")
+    todo.classList.add("todo")
+
+    const todoTitle = document.createElement("h3")
+    todoTitle.innerText = text
+    todo.appendChild(todoTitle)
+
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("finish-todo")
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement("button")
+    editBtn.classList.add("edit-todo")
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn)
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.classList.add("remove-todo")
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo);
+
+    todoInput.value = ''
+    todoInput.focus();
+}
+
+const toggleForms = () =>{
+    editForm.classList.toggle("hide")
+    todoform.classList.toglle("hide")
+    todoList.classList.toglle("hide")
+}
+
+//Eventos
+
+todoform.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+
+   const inputValue = todoInput.value
+
+   if(inputValue){
+    saveTodo(inputValue)  
+   }
+})
+
+
+document.addEventListener("click", (e) => {
+
+    const targetEl = e.target
+    const parentEl = targetEl.closest("div");
+
+    if(targetEl.classList.contains("finish-todo")){
+        parentEl.classList.toggle("Done")
+    }
+
+    if(targetEl.classList.contains("remove-todo")){
+        parentEl.remove();
+    }
+
+    if(targetEl.classList.contains("edit-todo")){
+        toggleForms()
+    }
+})
+
+cancelEditBtn.addEventListener("click", (e) =>{
+    e.preventDefault()
+
+    toggleForms();
+})
